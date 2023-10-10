ec30261f1bd60841c6495f31d6c5c9bc12d86e54
StevenU21 <ulloadeifheltsteven@gmail.com>
2023-10-10 13:45:40 -0600

    Fix: Migraciones

--- database/migrations/2023_10_10_193225_create_conversations_table.php
+++ database/migrations/2023_10_10_193225_create_conversations_table.php
@@ -14,14 +14,14 @@ return new class extends Migration
         Schema::create('conversations', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            // Relaciones con el sender y el receiver
-            $table->foreign('sender_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
-            $table->foreign('receiver_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
 
             // Persona que envía el mensaje
             $table->integer('sender_id')->unsigned();
             // Persona que recibe el mensaje
             $table->integer('receiver_id')->unsigned();
+            // Relaciones con el sender y el receiver
+            $table->foreign('sender_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('receiver_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
         });
     }
