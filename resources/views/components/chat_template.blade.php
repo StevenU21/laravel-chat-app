5b81de1d98be962d4080eb09119398ce7b914b3a
StevenU21 <ulloadeifheltsteven@gmail.com>
2023-10-29 21:07:41 -0600

    Avance Estilos para el Chat

--- resources/views/components/chat_template.blade.php
+++ resources/views/components/chat_template.blade.php
@@ -1,27 +1,23 @@
-@extends('layouts.panel')
-@section('title', 'Conversaciones')
-
-@section('content')
-    {{-- @livewire('chat.main') --}}
     <div class="card shadow-lg mx-4">
         <div class="card-body p-3">
             <div class="row gx-4">
                 <div class="col-auto">
                     <div class="avatar avatar-xl position-relative">
-                        <img src="{{ asset('img/team-1.jpg') }}" alt="profile_image" class="w-100 border-radius-lg shadow-sm">
+                        <img src="{{ asset('img/team-1.jpg') }}" alt="profile_image"
+                            class="w-100 border-radius-lg shadow-sm">
                     </div>
                 </div>
                 <div class="col-auto my-auto">
                     <div class="h-100">
                         <h5 class="mb-1">
-                            Celimar Castillo
+                            Sayo Kravits
                         </h5>
                         <p class="mb-0 font-weight-bold text-sm">
-                            Programación
+                            Public Relations
                         </p>
                     </div>
                 </div>
-                {{-- <div class="col-lg-4 col-md-6 my-sm-auto ms-sm-auto me-sm-0 mx-auto mt-3">
+                <div class="col-lg-4 col-md-6 my-sm-auto ms-sm-auto me-sm-0 mx-auto mt-3">
                     <div class="nav-wrapper position-relative end-0">
                         <ul class="nav nav-pills nav-fill p-1" role="tablist">
                             <li class="nav-item">
@@ -47,7 +43,7 @@
                             </li>
                         </ul>
                     </div>
-                </div> --}}
+                </div>
             </div>
         </div>
     </div>
@@ -56,20 +52,56 @@
             <div class="col-lg-4 col-md-5 col-12">
                 <div class="card blur shadow-blur max-height-vh-70 overflow-auto overflow-x-hidden mb-5 mb-lg-0">
                     <div class="card-header p-3">
-                        <h6>Conversaciones</h6>
-                        <input type="search" class="form-control" placeholder="Buscar Contacto" aria-label="search">
+                        <h6>Friends</h6>
+                        <input type="email" class="form-control" placeholder="Search Contact" aria-label="Email">
                     </div>
                     <div class="card-body p-2">
                         <a href="javascript:;" class="d-block p-2 border-radius-lg bg-gradient-primary">
                             <div class="d-flex p-2">
-                                <img alt="Image" src="{{ asset('img/team-3.jpg') }}" class="avatar shadow">
+                                <img alt="Image" src="{{ asset('img/team-2.jpg') }}" class="avatar shadow">
                                 <div class="ms-3">
                                     <div class="justify-content-between align-items-center">
-                                        <h6 class="text-white mb-0">Kevin Cardenas
+                                        <h6 class="text-white mb-0">Charlie Watson
                                             <span class="badge badge-success"></span>
                                         </h6>
-                                        <p class="text-white mb-0 text-sm">Escribiendo...</p>
+                                        <p class="text-white mb-0 text-sm">Typing...</p>
                                     </div>
+                                </div>
+                            </div>
+                        </a>
+                        <a href="javascript:;" class="d-block p-2">
+                            <div class="d-flex p-2">
+                                <img alt="Image" src="{{ asset('img/team-1.jpg') }}" class="avatar shadow">
+                                <div class="ms-3">
+                                    <h6 class="mb-0">Jane Doe</h6>
+                                    <p class="text-muted text-xs mb-2">1 hour ago</p>
+                                    <span class="text-muted text-sm col-11 p-0 text-truncate d-block">Computer users and
+                                        programmers</span>
+                                </div>
+                            </div>
+                        </a>
+                        <a href="javascript:;" class="d-block p-2">
+                            <div class="d-flex p-2">
+                                <img alt="Image" src="{{ asset('img/team-3.jpg') }}" class="avatar shadow">
+                                <div class="ms-3">
+                                    <h6 class="mb-0">Mila Skylar</h6>
+                                    <p class="text-muted text-xs mb-2">24 min ago</p>
+                                    <span class="text-muted text-sm col-11 p-0 text-truncate d-block">You can subscribe
+                                        to
+                                        receive
+                                        weekly...</span>
+                                </div>
+                            </div>
+                        </a>
+                        <a href="javascript:;" class="d-block p-2">
+                            <div class="d-flex p-2">
+                                <img alt="Image" src="{{ asset('img/team-5.jpg') }}" class="avatar shadow">
+                                <div class="ms-3">
+                                    <h6 class="mb-0">Sofia Scarlett</h6>
+                                    <p class="text-muted text-xs mb-2">7 hours ago</p>
+                                    <span class="text-muted text-sm col-11 p-0 text-truncate d-block">It’s an effective
+                                        resource
+                                        regardless..</span>
                                 </div>
                             </div>
                         </a>
@@ -77,10 +109,14 @@
                             <div class="d-flex p-2">
                                 <img alt="Image" src="{{ asset('img/team-4.jpg') }}" class="avatar shadow">
                                 <div class="ms-3">
-                                    <h6 class="mb-0">Celimar Castillo</h6>
-                                    <p class="text-muted text-xs mb-2">Hace 1 hora</p>
-                                    <span class="text-muted text-sm col-11 p-0 text-truncate d-block">Vas a ir Mañana al
-                                        Evento?</span>
+                                    <div class="justify-content-between align-items-center">
+                                        <h6 class="mb-0">Tom Klein</h6>
+                                        <p class="text-muted text-xs mb-2">1 day ago</p>
+                                    </div>
+                                    <span class="text-muted text-sm col-11 p-0 text-truncate d-block">Be sure to check
+                                        it
+                                        out if your
+                                        dev pro...</span>
                                 </div>
                             </div>
                         </a>
@@ -93,52 +129,56 @@
                         <div class="row">
                             <div class="col-lg-10 col-8">
                                 <div class="d-flex align-items-center">
-                                    <img alt="Image" src="{{ asset('img/team-3.jpg') }}" class="avatar">
+                                    <img alt="Image" src="{{ asset('img/team-2.jpg') }}" class="avatar">
                                     <div class="ms-3">
-                                        <h6 class="mb-0 d-block">Kevin Cardenas</h6>
-                                        <span class="text-sm text-dark opacity-8">Ultima vez activo a las 15:30</span>
+                                        <h6 class="mb-0 d-block">Charlie Watson</h6>
+                                        <span class="text-sm text-dark opacity-8">last seen today at 1:53am</span>
                                     </div>
                                 </div>
                             </div>
                             <div class="col-lg-1 col-2 my-auto pe-0">
-                                {{-- <button class="btn btn-icon-only shadow-none text-dark mb-0 me-3 me-sm-0" type="button"
-                                    data-bs-toggle="tooltip" data-bs-placement="top" title
+                                <button class="btn btn-icon-only shadow-none text-dark mb-0 me-3 me-sm-0"
+                                    type="button" data-bs-toggle="tooltip" data-bs-placement="top" title
                                     data-bs-original-title="Video call">
                                     <i class="ni ni-camera-compact"></i>
-                                </button> --}}
+                                </button>
                             </div>
-
                             <div class="col-lg-1 col-2 my-auto ps-0">
                                 <div class="dropdown">
                                     <button class="btn btn-icon-only shadow-none text-dark mb-0" type="button"
                                         data-bs-toggle="dropdown">
                                         <i class="ni ni-settings"></i>
                                     </button>
-                                    <ul class="dropdown-menu dropdown-menu-end me-sm-n2 p-2" aria-labelledby="chatmsg">
+                                    <ul class="dropdown-menu dropdown-menu-end me-sm-n2 p-2"
+                                        aria-labelledby="chatmsg">
                                         <li>
                                             <a class="dropdown-item border-radius-md" href="javascript:;">
-                                                <i class="ni ni-single-02 text-primary"></i> Ver Perfil
+                                                Profile
                                             </a>
                                         </li>
                                         <li>
                                             <a class="dropdown-item border-radius-md" href="javascript:;">
-                                                <i class="ni ni-user-run text-danger"></i> Bloquear
+                                                Mute conversation
                                             </a>
                                         </li>
                                         <li>
                                             <a class="dropdown-item border-radius-md" href="javascript:;">
-                                                <i class="ni ni-archive-2 text-success"></i> Vaciar Chat
+                                                Block
                                             </a>
                                         </li>
                                         <li>
                                             <a class="dropdown-item border-radius-md" href="javascript:;">
-                                                <i class="ni ni-fat-remove text-danger"></i> Borrar Conversación
+                                                Clear chat
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a class="dropdown-item border-radius-md text-danger" href="javascript:;">
+                                                Delete chat
                                             </a>
                                         </li>
                                     </ul>
                                 </div>
                             </div>
-
                         </div>
                     </div>
                     <div class="card-body overflow-auto overflow-x-hidden">
@@ -162,7 +202,8 @@
                                 <div class="card bg-gray-200">
                                     <div class="card-body py-2 px-3">
                                         <p class="mb-1">
-                                            Can it generate daily design links that include essays and data visualizations
+                                            Can it generate daily design links that include essays and data
+                                            visualizations
                                             ?<br>
                                         </p>
                                         <div class="d-flex align-items-center justify-content-end text-sm opacity-6">
@@ -208,7 +249,37 @@
                                 </div>
                             </div>
                         </div>
-
+                        <div class="row justify-content-start mb-4">
+                            <div class="col-auto">
+                                <div class="card ">
+                                    <div class="card-body py-2 px-3">
+                                        <p class="mb-1">
+                                            You can easily get it; The content here is all free
+                                        </p>
+                                        <div class="d-flex align-items-center text-sm opacity-6">
+                                            <i class="ni ni-check-bold text-sm me-1"></i>
+                                            <small>4:42pm</small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row justify-content-end text-right mb-4">
+                            <div class="col-auto">
+                                <div class="card bg-gray-200">
+                                    <div class="card-body py-2 px-3">
+                                        <p class="mb-1">
+                                            Awesome, blog is important source material for anyone who creates apps? <br>
+                                            Beacuse these blogs offer a lot of information about website development.
+                                        </p>
+                                        <div class="d-flex align-items-center justify-content-end text-sm opacity-6">
+                                            <i class="ni ni-check-bold text-sm me-1"></i>
+                                            <small>4:42pm</small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                         <div class="row justify-content-start mb-4">
                             <div class="col-5">
                                 <div class="card ">
@@ -245,7 +316,7 @@
                                 <div class="card ">
                                     <div class="card-body py-2 px-3">
                                         <p class="mb-0">
-                                            Kevin Cardenas Está Escribiendo...
+                                            Charlie is Typing...
                                         </p>
                                     </div>
                                 </div>
@@ -256,7 +327,7 @@
                         <form class="align-items-center">
                             <div class="d-flex">
                                 <div class="input-group">
-                                    <input type="text" class="form-control" placeholder="Escribe Algo..."
+                                    <input type="text" class="form-control" placeholder="Type here"
                                         aria-label="Message example input">
                                 </div>
                                 <button class="btn bg-gradient-primary mb-0 ms-2">
@@ -269,4 +340,3 @@
             </div>
         </div>
     </div>
-@endsection
